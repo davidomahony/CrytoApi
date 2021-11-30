31014fc95e91935cf1159048825f96080f88076f
David O Mahony <dave.omahony@betwaygroup.com>
2021-11-30 15:40:43 +0000

    Adding template projects

---
+++ CrytptoInfo.POCWork/Program.cs
@@ -0,0 +1,38 @@
+﻿using Binance.Net;
+using Binance.Net.Objects;
+using CryptoExchange.Net.Authentication;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace CrytptoInfo.POCWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, new Uri("https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id=2&convertId=2781&timeStart=1535040000&timeEnd=1738144000"));
+            request.Headers.Add("authority", "api.coinmarketcap.com");
+            request.Headers.Add("path", "/data-api/v3/cryptocurrency/historical?id=1&convertId=2781&timeStart=1627516800&timeEnd=1638144000");
+            request.Headers.Add("authority", "api.coinmarketcap.com");
+            request.Headers.Add("Accept", "*/*");
+            request.Headers.Add("User-Agent", "PostmanRuntime/7.28.4");
+            request.Headers.Add("Connection", "keep-alive");
+
+            var result = client.Send(request);
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var responseContent = result.Content.ReadAsStringAsync().Result;
+                var deserializedResponse = JsonConvert.DeserializeObject<Response>(responseContent);
+
+                if (deserializedResponse != null)
+                {
+                    var test = deserializedResponse;
+                }
+            }
+        }
+    }
+}
---
+++ CrytptoInfo.POCWork/Response.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrytptoInfo.POCWork
+{
+    public class Response
+    {
+        public DataObject Data { get; set; }
+
+        public class DataObject
+        {
+            public string Id { get; set; }
+
+            public string name { get; set; }
+            public string Symbol { get; set; }
+
+            public IEnumerable<QuoteInformation> Quotes { get; set; }
+
+            public class QuoteInformation
+            {
+                public DateTime TimeOpen { get; set; }
+                public DateTime TimeClose { get; set; }
+                public DateTime TimeLow { get; set; }
+                public DateTime TimeHigh { get; set; }
+
+                public QuoteObject Quote { get; set; }
+
+                public class QuoteObject
+                {
+                    public float Open { get; set; }
+                    public float High { get; set; }
+                    public float Low { get; set; }
+                    public float Close { get; set; }
+                    public float Volume { get; set; }
+                    public float MarketCap { get; set; }
+                    public DateTime TimeStamp { get; set; }
+                }
+            }
+            // {"timeOpen":"2018-08-24T00:00:00.000Z","timeClose":"2018-08-24T23:59:59.999Z","timeHigh":"2018-08-24T17:29:00.000Z","timeLow":"2018-08-24T14:49:01.000Z",
+            // "quote":{"open":57.6042000000,"high":58.2782000000,"low":55.9536000000,"close":57.9298000000,"volume":194676000.0000000000,"marketCap":3359443512.6700000000,"timestamp":"2018-08-24T23:59:59.999Z"}}
+        }
+    }
+}
