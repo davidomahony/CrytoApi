78414a6ae370a655bed19588d96c55d28ab1dd0e
David O Mahony <dave.omahony@betwaygroup.com>
2021-12-10 11:17:41 +0000

    Added basic POC for fetching tweets from a specific user

--- CrytptoInfo.POCWork/Program.cs
+++ CrytptoInfo.POCWork/Program.cs
@@ -13,6 +13,28 @@ namespace CrytptoInfo.POCWork
         {
             var client = new HttpClient();
 
+            var request = new HttpRequestMessage(HttpMethod.Get, new Uri("https://api.twitter.com/2/users/44196397/tweets?max_results=10&tweet.fields=created_at,public_metrics"));
+            request.Headers.Add("Authorization", "Bearer YouWillNeverGetThis");
+
+            var result = client.Send(request);
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var responseContent = result.Content.ReadAsStringAsync().Result;
+                var deserializedResponse = JsonConvert.DeserializeObject<TwitterResponse>(responseContent);
+
+                if (deserializedResponse != null)
+                {
+                    Console.WriteLine("Success");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private void HistoricalDataTest()
+        {
+            var client = new HttpClient();
+
             var request = new HttpRequestMessage(HttpMethod.Get, new Uri("https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id=2&convertId=2781&timeStart=1535040000&timeEnd=1738144000"));
             request.Headers.Add("authority", "api.coinmarketcap.com");
             request.Headers.Add("path", "/data-api/v3/cryptocurrency/historical?id=1&convertId=2781&timeStart=1627516800&timeEnd=1638144000");
