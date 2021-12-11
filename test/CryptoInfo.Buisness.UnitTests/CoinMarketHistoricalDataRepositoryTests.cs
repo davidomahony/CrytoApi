24034b4e90238959583e40fe8c8f49b1debc314d
David O Mahony <dave.omahony@betwaygroup.com>
2021-12-11 18:19:01 +0000

    Changed cirectory

---
+++ test/CryptoInfo.Buisness.UnitTests/CoinMarketHistoricalDataRepositoryTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CryptoInfo.Buisness.UnitTests
+{
+    [TestClass]
+    public class CoinMarketHistoricalDataRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
